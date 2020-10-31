c6214d2f337f6886bf6aa60220444f794102e5a2
simonyjs <cage@live.co.kr>
2020-10-31 12:25:38 +0900

    2.0 Creating your first React Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Potato from './Potato';
 
 function App() {
-  return <div>Hello World!!!</div>;
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+      <h2>This is my first React APP!</h2>
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Potato.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Potato() {
+  return <h4>I Love potato!!!</h4>;
+}
+
+export default Potato;
